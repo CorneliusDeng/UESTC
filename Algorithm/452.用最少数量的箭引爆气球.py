b31ac6459ff5d20fd162a6e45db797b0a8ce558c
CorneliusDeng <corneliusdeng@163.com>
2023-07-11 20:29:37 +0800

    test

---
+++ Algorithm/452.用最少数量的箭引爆气球.py
@@ -0,0 +1,15 @@
+#
+# @lc app=leetcode.cn id=452 lang=python
+#
+# [452] 用最少数量的箭引爆气球
+#
+
+# @lc code=start
+class Solution(object):
+    def findMinArrowShots(self, points):
+        """
+        :type points: List[List[int]]
+        :rtype: int
+        """
+# @lc code=end
+
