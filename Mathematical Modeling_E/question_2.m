29662edde9126220b72b4e5d3053b2e50997706d
CorneliusDeng <corneliusdeng@163.com>
2023-09-24 21:03:59 +0800

    Math 9-24 well-done

--- Mathematical Modeling_E/question_2.m
+++ Mathematical Modeling_E/question_2.m
@@ -7,88 +7,88 @@ Num=unique(Table(2:end,1),'stable');
 T=[]; % 记录复查时间
 ED=[]; % 记录水肿数据
 A=[]; % 记录除此诊断信息
-for i=1:length(Num)
-    a=find(Table(:,1)==Num(i));
-    A=[A;double(Table(a(1),4:end))];
-    T=[T;double(Table(a,3))];
-    ED=[ED;double(Table(a,34))];
+for i=1:length(Num) % 循环遍历每个病人
+    a=find(Table(:,1)==Num(i)); % 找到该病人的所有数据
+    A=[A;double(Table(a(1),4:end))]; % 记录该病人的除此诊断信息
+    T=[T;double(Table(a,3))]; % 记录该病人的复查时间
+    ED=[ED;double(Table(a,34))]; % 记录该病人的水肿数据
 end
-figure
-plot(T,ED,'*')
+figure % 新建一个图形窗口
+plot(T,ED,'*') % 绘制散点图
 xlim([1,2000])
 % 高斯模型
-hold on
-gaussModel = fit(T, ED, 'gauss1')
-plot(gaussModel)
+hold on % 保持当前图形窗口，并在其上绘制新图形
+gaussModel = fit(T, ED, 'gauss1') % 拟合高斯模型
+plot(gaussModel) % 绘制高斯模型曲线
 xlabel('时间')
 ylabel('水肿/10^-3ml')
 % 计算残差
-ED_fit=gaussModel(T);
-Error=[];
-for i=1:length(Num)
-    a=find(Table(:,1)==Num(i))-1;
-    Error(i,1)=mean(abs(ED_fit(a)-ED(a)));
+ED_fit=gaussModel(T); % 计算拟合值
+Error=[]; % 记录残差
+for i=1:length(Num) % 循环遍历每个病人
+    a=find(Table(:,1)==Num(i))-1; % 找到该病人的所有数据的前一行
+    Error(i,1)=mean(abs(ED_fit(a)-ED(a))); % 计算残差
 end
 
 %% b
-%个体差异用Tabel第4-15列的指标来进行聚类
-cluster_n=5;%聚类中心
-[center, U, obj_fcn] = fcm(double(A(:,1:12)), cluster_n);
-figure%目标函数变化值
+% 个体差异用Tabel第4-15列的指标来进行聚类
+cluster_n=5; % 聚类中心
+[center, U, obj_fcn] = fcm(double(A(:,1:12)), cluster_n); % 模糊C均值聚类
+figure
 plot(obj_fcn)
 xlabel('iteration')
 ylabel('obj.fcn_value')
 title('FCM聚类')
-[~,u]=max(U);%所属亚类
-ED_fit2=[];
-for i=1:cluster_n
-    a=find(u==i);
-    b=find(ismember(Table(2:end,1),Num(a))==1);
+[~,u]=max(U); % 所属亚类
+ED_fit2=[]; % 记录拟合值
+for i=1:cluster_n % 循环遍历每个亚类
+    a=find(u==i); % 找到属于该亚类的病人编号
+    b=find(ismember(Table(2:end,1),Num(a))==1); % 找到属于该亚类的数据
     figure
     plot(T(b),ED(b),'*')
     xlim([1,2000])
-    %高斯模型
+    % 高斯模型
     hold on
     disp('亚类1：')
-    gaussModel = fit(T(b), ED(b), 'gauss1')
-    plot(gaussModel)
+    gaussModel = fit(T(b), ED(b), 'gauss1') % 拟合高斯模型
+    plot(gaussModel) % 绘制高斯模型曲线
     xlabel('时间')
     ylabel('水肿/10^-3ml')
     title(['亚类',num2str(i)])
-    ED_fit2(b,1)=gaussModel(T(b));
+    ED_fit2(b,1)=gaussModel(T(b)); % 计算拟合值
 end
-%计算残差
+% 计算残差
 Error2=[];
-for i=1:length(Num)
-    a=find(Table(:,1)==Num(i))-1;
-    Error2(i,1)=mean(abs(ED_fit2(a)-ED(a)));
+for i=1:length(Num) % 循环遍历每个病人
+    a=find(Table(:,1)==Num(i))-1; % 找到该病人的所有数据的前一行
+    Error2(i,1)=mean(abs(ED_fit2(a)-ED(a))); % 计算残差
 end
 result2=[["患者","残差(全体)","残差(亚类)","亚类"];[Num,Error,Error2,u']];
 
 %% c
-%计算水肿指标的变化率，在不同治疗方法下，改善效率
-K=[];
-for i=1:length(Num)
-    a=find(Table(:,1)==Num(i));
-    if length(a)>=5%至少五次检查数据才进行计算
-        k=(double(Table(a(2:end),34))-double(Table(a(1:end-1),34)))./(double(Table(a(2:end),3))-double(Table(a(1:end-1),3)));
-        kk=find(k<0);
+% 计算水肿指标的变化率，在不同治疗方法下，改善效率
+K=[]; % 初始化变化率
+for i=1:length(Num) % 循环遍历每个病人
+    a=find(Table(:,1)==Num(i)); % 找到该病人的所有数据
+    if length(a)>=5 % 至少五次检查数据才进行计算
+        k=(double(Table(a(2:end),34))-double(Table(a(1:end-1),34)))./(double(Table(a(2:end),3))-double(Table(a(1:end-1),3))); % 计算变化率
+        kk=find(k<0); % 找到变化率小于0的数据
         if length(kk)==0
-            K(i,1)=0;
+            K(i,1)=0; % 如果没有变化率小于0的数据，则变化率为0
         else
-            K(i,1)=mean(k(kk));
+            K(i,1)=mean(k(kk)); % 计算变化率的平均值
         end
     else
-        K(i,1)=NaN;
+        K(i,1)=NaN; % 如果数据不足五次，则变化率为NaN
     end
 end
-c=setdiff([1:length(K)],find(isnan(K)==1));
-G=double(Table(2:end,16:22));
-Z=Table(1,16:22);
-for i=1:length(Z)
-    [p(i),anovatab{i},stats{i}]=anova1(K(c),G(c,i)+1,'off');%单因素方差分析
-    fa=finv(0.95,anovatab{i}{2,3},anovatab{i}{3,3});%计算fa
-    F=anovatab{i}{2,5};%F值
+c=setdiff([1:length(K)],find(isnan(K)==1)); % 找到变化率不为NaN的数据的索引
+G=double(Table(2:end,16:22)); % 获取治疗方法数据
+Z=Table(1,16:22); % 获取治疗方法名称
+for i=1:length(Z) % 循环遍历每种治疗方法
+    [p(i),anovatab{i},stats{i}]=anova1(K(c),G(c,i)+1,'off'); % 单因素方差分析
+    fa=finv(0.95,anovatab{i}{2,3},anovatab{i}{3,3}); % 计算fa
+    F=anovatab{i}{2,5}; % F值
     if p(i)<=0.01 && F>fa
         disp([Z(i)+"对水肿体积进展模式影响非常显著"])
         fprintf('p值为%.4f<0.01，F值为%.2f>%.2f\n',p(i),F,fa)
@@ -106,28 +106,28 @@ str=Z(q)+"">"";
 disp(str)
 
 %% d
-%同上述步骤求血肿体积的
-%计算水肿指标的变化率，在不同治疗方法下，改善效率
-K2=[];
-for i=1:length(Num)
-    a=find(Table(:,1)==Num(i));
-    if length(a)>=5%至少五次检查数据才进行计算
-        k=(double(Table(a(2:end),23))-double(Table(a(1:end-1),23)))./(double(Table(a(2:end),3))-double(Table(a(1:end-1),3)));
-        kk=find(k<0);
+% 同上述步骤求血肿体积的
+% 计算水肿指标的变化率，在不同治疗方法下，改善效率
+K2=[]; % 初始化变化率
+for i=1:length(Num) % 循环遍历每个病人
+    a=find(Table(:,1)==Num(i)); % 找到该病人的所有数据
+    if length(a)>=5 % 至少五次检查数据才进行计算
+        k=(double(Table(a(2:end),23))-double(Table(a(1:end-1),23)))./(double(Table(a(2:end),3))-double(Table(a(1:end-1),3))); % 计算变化率
+        kk=find(k<0); % 找到变化率小于0的数据
         if length(kk)==0
-            K2(i,1)=0;
+            K2(i,1)=0; % 如果没有变化率小于0的数据，则变化率为0
         else
-            K2(i,1)=mean(k(kk));
+            K2(i,1)=mean(k(kk)); % 计算变化率的平均值
         end
     else
-        K2(i,1)=NaN;
+        K2(i,1)=NaN; % 如果数据不足五次，则变化率为NaN
     end
 end
-c=setdiff([1:length(K2)],find(isnan(K2)==1));
-for i=1:length(Z)
-    [p2(i),anovatab2{i},stats2{i}]=anova1(K2(c),G(c,i)+1,'off');%单因素方差分析
-    fa=finv(0.95,anovatab2{i}{2,3},anovatab2{i}{3,3});%计算fa
-    F=anovatab2{i}{2,5};%F值
+c=setdiff([1:length(K2)],find(isnan(K2)==1)); % 找到变化率不为NaN的数据的索引
+for i=1:length(Z) % 循环遍历每种治疗方法
+    [p2(i),anovatab2{i},stats2{i}]=anova1(K2(c),G(c,i)+1,'off'); % 单因素方差分析
+    fa=finv(0.95,anovatab2{i}{2,3},anovatab2{i}{3,3}); % 计算fa
+    F=anovatab2{i}{2,5}; % F值
     if p2(i)<=0.01 && F>fa
         disp([Z(i)+"对血肿肿体积进展模式影响非常显著"])
         fprintf('p值为%.4f<0.01，F值为%.2f>%.2f\n',p2(i),F,fa)
@@ -143,10 +143,10 @@ disp('不同治疗对血肿进展模式的影响大小为：')
 [~,q2]=sort(p2);
 str2=Z(q2)+"">"";
 disp(str2)
-%在算下血肿指标与水肿指标的相关性
-x0=double(Table(2:end,23));
-y0=double(Table(2:end,34));
-theta=x0'*y0/(norm(x0)*norm(y0));%余弦相似度
+% 计算血肿指标与水肿指标的相关性
+x0=double(Table(2:end,23)); % 获取血肿数据
+y0=double(Table(2:end,34)); % 获取水肿数据
+theta=x0'*y0/(norm(x0)*norm(y0)); % 余弦相似度
 fprintf('血肿指标与水肿指标的相关度为：%.4f\n',theta)
 
 disp('结果见矩阵result2')
--- Mathematical Modeling_E/question_3.m
+++ Mathematical Modeling_E/question_3.m
@@ -1,30 +1,37 @@
 clear
 clc
-%是否要进行指标降维自行添加
+
 load data
-Num=unique(Table(2:end,1),'stable');
-A=[];%首检
-Y=[];%首检Rms
-B=[];%随检
-N1=[];
-N2=[];
-Z=[Table(1,[4:end])];
-for i=1:length(Num)
-    a=find(Table(:,1)==Num(i));
-    A=[A;double(Table(a(1),[4:end]))];
-    N1=[N1;Table(a(1),1:2)];
-    B=[B;double(Table(a(2:end),[4:end]))];
-    N2=[N2;Table(a(2:end),1:2)];
+Num = unique(Table(2:end,1),'stable');
+A = []; % 首检
+Y = []; % 首检Rms
+B = []; % 随检
+N1 = [];
+N2 = [];
+Z = [Table(1,[4:end])];
+for i = 1:length(Num)
+    a = find(Table(:,1) == Num(i));
+    A = [A; double(Table(a(1),[4:end]))];
+    N1 = [N1; Table(a(1),1:2)];
+    B = [B; double(Table(a(2:end),[4:end]))];
+    N2 = [N2; Table(a(2:end),1:2)];
 end
+
 %% a
-In=A;
-Y=xlsread('表1-患者列表及临床信息.xlsx','B2:B101');
-Out=Y;
-%bp神经网络
-Xtrain = In(1:100,:);
+In = A;
+Y = xlsread('表1-患者列表及临床信息.xlsx','B2:B101');
+Out = Y;
+
+% 数据降维（PCA）
+numComponents = 10; % 设置降维后的维数
+[coeff, score, ~, ~, explained] = pca(In);
+In_pca = score(:, 1:numComponents);
+
+Xtrain = In_pca(1:100,:);
 Ytrain = Out;
-Xtest1 = In(101:130,:);
-Xtest2 = In(131:160,:);
+Xtest1 = In_pca(101:130,:);
+Xtest2 = In_pca(131:160,:);
+
 %%  训练模型
 trees = 100;                                      % 决策树数目
 leaf  = 5;                                        % 最小叶子数
@@ -33,48 +40,51 @@ OOBPredictorImportance = 'on';                    % 计算特征重要性
 Method = 'classification';                            % 分类还是回归
 net = TreeBagger(trees, Xtrain, Ytrain, 'OOBPredictorImportance', OOBPredictorImportance,...
       'Method', Method, 'OOBPrediction', OOBPrediction, 'minleaf', leaf);
-%预测
-Y=string(predict(net,Xtrain));
-Y1=string(predict(net,Xtest1));
-Y2=string(predict(net,Xtest2));
-%混淆矩阵
+
+% 预测
+Y = string(predict(net,Xtrain));
+Y1 = string(predict(net,Xtest1));
+Y2 = string(predict(net,Xtest2));
+
+% 混淆矩阵
 figure
-plotconfusion(categorical(Ytrain),categorical(Y));
+plotconfusion(categorical(Ytrain), categorical(Y));
 title('训练集')
 figure
 hold on
-auc=plot_roc(double(Y)',Ytrain');
+auc = plot_roc(double(Y)', Ytrain');
 legend(['训练集 auc=',num2str(round(auc,2))])
+
 %% b
 Xyuce = B;
-Y3 = string(predict(net,Xyuce));
-result3="""";
-for i=1:length(Num)
-    a=find(N2==Num(i));
-    for j=1:length(a)
-        result3(i,j)=Y3(a(j));
+In_pca_yuce = (Xyuce - mean(In)) * coeff(:,1:numComponents); % 在预测集上应用相同的PCA转换
+Y3 = string(predict(net, In_pca_yuce));
+
+result3 = "";
+for i = 1:length(Num)
+    a = find(N2 == Num(i));
+    for j = 1:length(a)
+        result3(i,j) = Y3(a(j));
     end
 end
-result3(ismissing(result3)==1)="""";
-z=[];
-for i=1:size(result3,2)
-    z=[z,"第"+num2str(i)+"次随访"];
+result3(ismissing(result3) == 1) = "";
+z = [];
+for i = 1:size(result3,2)
+    z = [z,"第"+num2str(i)+"次随访"];
 end
-result3=[["患者","首次检测";Num,[Y;Y1;Y2]],[z;result3]];
+result3 = [["患者","首次检测";Num,[Y;Y1;Y2]],[z;result3]];
+
 %% c
-%相关性分析
-X=double(Table(2:end,[4:end]));
-P=[];
-for i=1:size(X,2)
-    for j=1:size(X,2)
-        P(i,j)=X(:,i)'*X(:,j)/(norm(X(:,i))*norm(X(:,j)));
-    end
-end
-figure%热图
+% 相关性分析
+X = double(Table(2:end,[4:end]));
+P = corrcoef(X);
+
+figure % 热图
 set(gca,'position',[0.05 0.05 0.9 0.9])
-h=heatmap(P,'ColorbarVisible', 'on');
+h = heatmap(P,'ColorbarVisible', 'on');
 h.FontSize = 8;
 h.CellLabelFormat = '%0.2g';
-resultp=["""",Z;Z',P];%自行分析相关性结果
+
+resultp = ["",Z;Z',P]; % 自行分析相关性结果
 
 disp('预测结果见result3，相关性结果见resultp')
